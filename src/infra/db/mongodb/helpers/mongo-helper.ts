a83170622509db0a27f494e07cd2b683892b2119
brunosm16 <brunoskr23@gmail.com>
2021-12-13 19:39:16 -0300

    refactor: add type

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,8 +1,8 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export abstract class MongoHelper {
-  private static connection
+  private static connection: MongoClient
 
   public static async connect (uri: string): Promise<void> {
     this.connection = await MongoClient.connect(uri, {
@@ -13,5 +13,9 @@ export abstract class MongoHelper {
 
   public static async disconnect (): Promise<void> {
     await this.connection.close()
+  }
+
+  public static getCollection (name: string): Collection {
+    return this.connection.db().collection(name)
   }
 }

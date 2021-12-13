efc87793cefcb9b1fe87af7a3e4507f192bf8408
brunosm16 <brunoskr23@gmail.com>
2021-12-13 18:42:28 -0300

    fix: change test environment to avoid conflicts

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
 module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
+  watchPathIgnorePatterns: ['globalConfig'],
   preset: '@shelf/jest-mongodb'
 }

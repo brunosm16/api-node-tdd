c9ad371d40df1ffb9102f362e7043de15fad8700
brunosm16 <brunoskr23@gmail.com>
2021-12-20 09:00:40 -0300

    refactor: returns error message as body

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,7 +4,7 @@ import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: MissingParamError): HttpResponse => ({
   statusCode: 400,
-  body: error
+  body: error.message
 })
 
 export const serverError = (): HttpResponse => ({
